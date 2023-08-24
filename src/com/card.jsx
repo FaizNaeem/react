6bd3a9a204e2862779a4752da73acb3d69760c94
FaizNaeem <faiznaeem192022@gmail.com>
2023-08-24 17:19:41 +0500

    react

---
+++ src/com/card.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+
+
+export default class card extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
