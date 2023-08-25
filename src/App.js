a804d872f575f71fb323209042e4996a0bc63b6e
FaizNaeem <faiznaeem192022@gmail.com>
2023-08-25 23:22:16 +0500

    react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+// import cardard from './com/card';
 import ResponsiveAppBar from './com/Appbar';
 import Hi from './com/slider';
+import Card from './com/card';
 
 function App() {
   return (
     <div className="App">
   <ResponsiveAppBar />
   <Hi/>
+  <Card />
     </div>
   );
 }
--- src/com/card.jsx
+++ src/com/card.jsx
@@ -3,11 +3,18 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 
 
-export default class card extends Component {
+export default class Card extends Component {
   render() {
     return (
       <div>
-
+<div class="card"  >
+  <img src="." class="card-img-top" alt="..." />
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="hi" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
       </div>
     )
   }
