7d1e6ec19f9638702fec7959def3dcd0c0f088d6
FaizNaeem <faiznaeem192022@gmail.com>
2023-08-29 18:57:33 +0500

    react

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import sum from './name';
+import data from './name';
+import React from 'react';
+import Hello from './hello';
 // import cardard from './com/card';
-import ResponsiveAppBar from './com/Appbar';
-import Hi from './com/slider';
-import Card from './com/card';
+// import ResponsiveAppBar from './com/Appbar';
+// import Hi from './com/slider';
+// import Card from './com/card';
+// impo Hello
 
 function App() {
   return (
-    <div className="App">
-  <ResponsiveAppBar />
-  <Hi/>
-  <Card />
-    </div>
-  );
+    
+    data.map((e)=>{
+     return <Hello 
+      title={e.title}/>
+     
+      // console.log(e);
+    })
+
+    )
 }
 
 export default App;
---
+++ src/name.jsx
@@ -0,0 +1,26 @@
+
+
+
+
+
+// function sum (a,b){
+//     return(
+//         a+b
+//     )
+// }
+// export default sum
+const data = [
+    {
+    title:"faiz"
+},
+{
+title:"faiz1"
+},
+{
+title:"faiz2"
+}
+,    {
+    title:"faiz3"
+}
+]
+export default data
