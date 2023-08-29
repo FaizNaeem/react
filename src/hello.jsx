1d476250091f1d5a9662a17128dd3f23351a7f1e
FaizNaeem <faiznaeem192022@gmail.com>
2023-08-29 18:58:35 +0500

    Hello

--- src/hello.jsx
+++ src/hello.jsx
@@ -2,7 +2,7 @@ import React from "react";
 function Hello(props){
     return(
         <div>
-            <h1>
+            <h1>Hello
                 {props.title}
             </h1>
         </div>
